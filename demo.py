e6eb0c80ed61b1f9649d9b6c0b8e37e14567fd86
dima <dprofe@yandex.ru>
2022-12-05 19:53:07 +0300

    Initial commit

---
+++ demo.py
@@ -0,0 +1,83 @@
+import random
+
+from server import Server, Room
+
+
+class MyRoom(Room):
+    def __init__(self, game_name):
+        super().__init__(game_name)
+        self.guessed_num = random.randint(0, 100)
+
+
+class MyServer(Server):
+    def __init__(self, host, port):
+        super().__init__(host, port, MyRoom)
+
+    def on_disconnect(self, sid):
+        self.leave_room(sid, self.get_session(sid).get('room'))
+
+    @Server.sio_event('MyServer', 'check_number')
+    def guess(self, sid, data):
+        print(f'{self.get_session(sid)["login"]} думает: {data["n"]}')
+        room = self.get_room_by_number(self.get_session(sid)['room'])
+
+        if room.guessed_num > data['n']:
+            self.emit('answer', {'result': 'greater'}, sid)
+        elif room.guessed_num < data['n']:
+            self.emit('answer', {'result': 'less'}, sid)
+        else:
+            self.emit('answer', {'result': 'equals'}, sid)
+            self.emit(
+                'winner',
+                {
+                    'login': self.get_session(sid)['login'],
+                    'n': room.guessed_num
+                },
+                room=room.number,
+                skip_sid=sid
+            )
+
+    @Server.sio_event('MyServer', 'join')
+    def join(self, sid, data):
+        ans = {
+            'created': False,
+            'joined': True
+        }
+        room = self.get_room_by_name(data["game_id"])
+        if room is None:
+            room = MyRoom(data['game_id'])
+            self.rooms.append(room)
+            ans['created'] = True
+
+            print(f'Создана комната с id="{data["game_id"]}"')
+            print(f'Пользователь {data["login"]} создал её')
+        else:
+            print(f'Пользователь {data["login"]} пытается присоединиться к {data["game_id"]}')
+            if room.closed:
+                print('Комната закрыта')
+                ans['joined'] = False
+            else:
+                print('Успешно')
+
+        if not ans['joined']:
+            self.save_session(sid, {'login': data['login'], 'room': None})
+        else:
+            self.enter_room(sid, room.number)
+            self.save_session(sid, {'login': data['login'], 'room': room.number})
+            self.emit('new_user', {'login': data['login']}, room=room.number, skip_sid=sid)
+
+        self.emit('join_answer', ans, sid)
+
+    @Server.sio_event('MyServer', 'close_room')
+    def close_room(self, sid, data):
+        room = self.get_session(sid).get('room')
+        if room is None:
+            return
+        self.get_room_by_number(room).close()
+
+        self.emit('start_game', {})
+
+
+srv = MyServer('0.0.0.0', 5000)
+
+srv.run()
---
+++ server.py
@@ -0,0 +1,113 @@
+import functools
+from collections import defaultdict
+
+import socketio
+import eventlet
+
+
+class Room:
+    next_number = 1
+
+    def __init__(self, name):
+        self.closed = False
+        self.number = Room.next_number
+        self.name = name
+        self.users_count = 0
+        Room.next_number += 1
+
+    def close(self):
+        self.closed = True
+
+
+class Server:
+    _methods = defaultdict(list)
+
+    def __init__(self, host, port, room_class=Room):
+        self.room_class = room_class
+        self.host = host
+        self.port = port
+        self.sio = socketio.Server()
+        self.app = socketio.WSGIApp(self.sio)
+
+        self.events_data = defaultdict(lambda: False)
+        self.rooms = []
+
+        self._on_connect = self.sio.on('connect')(self._on_connect)
+        self._on_disconnect = self.sio.on('disconnect')(self._on_disconnect)
+        self._on_any_event = self.sio.on('*')(self._on_any_event)
+
+        for method, event in Server._methods[self.__class__.__name__]:
+            setattr(self, method, self.sio.on(event)(getattr(self, method)))
+
+    def get_room_by_number(self, num):
+        for r in self.rooms:
+            if r.number == num:
+                return r
+        return None
+
+    def get_room_by_name(self, name):
+        for r in self.rooms:
+            if r.name == name:
+                return r
+        return None
+
+    def on_connect(self, sid, environ):
+        pass
+
+    def _on_connect(self, sid, environ):
+        pass
+
+    def _on_disconnect(self, sid):
+        room_n = self.get_session(sid).get('room')
+        if room_n is not None:
+            self.leave_room(sid, room_n)
+
+    def on_disconnect(self, sid):
+        pass
+
+    def _on_any_event(self, event, sid, data):
+        self.events_data[event] = True
+        self.on_any_event(event, sid, data)
+
+    def on_any_event(self, event, sid, data):
+        pass
+
+    def emit(self, evt, data, to=None, room=None, skip_sid=None):
+        self.sio.emit(evt, data, to, room, skip_sid)
+
+    def enter_room(self, sid, room):
+        rm = self.get_room_by_number(room)
+        if rm is not None:
+            rm.users_count += 1
+
+        self.sio.enter_room(sid, room)
+
+    def leave_room(self, sid, room):
+        rm = self.get_room_by_number(room)
+        if rm is not None:
+            rm.users_count -= 1
+            if rm.users_count <= 0 and rm.closed:
+                self.rooms.remove(rm)
+        self.sio.leave_room(sid, room)
+
+    def get_session(self, sid):
+        return self.sio.get_session(sid)
+
+    def save_session(self, sid, session):
+        self.sio.save_session(sid, session)
+
+    def run(self):
+        eventlet.wsgi.server(eventlet.listen(('', 5000)), self.app)
+
+    @staticmethod
+    def sio_event(class_name, event):
+        def decor(method):
+            Server._methods[class_name].append((method.__name__, event))
+
+            @functools.wraps(method)
+            def wrapper(*a, **kw):
+                return method(*a, **kw)
+
+            return wrapper
+
+        return decor
